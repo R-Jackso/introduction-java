23ef27a330edbc2ae42c79a73539a245bc3eb7fd
Oliver Rich-Jackson <10Oliver28@gmail.com>
2026-06-23 13:18:07 -0600

    Add Introduction program

---
+++ Introduction.java
@@ -0,0 +1,7 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("\nHello and good evening,");
+        System.out.println("\nI am the first programme created in a curriculum designed to help my creator learn java");
+        System.out.println("By the end, he will be more than ready to face year one of a computer science degree at Trinity College Dublin\n");
+    }
+}
