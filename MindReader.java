7adf5542a770aa31eef8819ba2c8e1d9fe5d38b0
Oliver Rich-Jackson <10Oliver28@gmail.com>
2026-06-25 16:40:22 -0600

    Add framework for a mind reader game for control flow lesson

---
+++ MindReader.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+"""
+### Planning ###
+
+Introduce the game (on first run. skip on subsequent runs using do while) and ask for player's number
+
+set high int
+set low int
+
+Begin loop (if high and low are good. If not chastise and restart game)
+    Ask player about guess
+    
+    if correct:
+        win message + restart
+        restart loop
+    elif low:
+        adjust low
+    elif high:
+        adjust high
+        restart loop
+    else (invalid):
+        instruct player on rules
+        restart loop (with original guess)
+        
+"""
+
+public class MindReader {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
