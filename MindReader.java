be722bbcb33fa8681783f5ede3f996d5b3bbb338
Oliver Rich-Jackson <10Oliver28@gmail.com>
2026-06-25 18:25:08 -0600

    Finish MindReader program

--- MindReader.java
+++ MindReader.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
-
-"""
-### Planning ###
+/*
+Planning
 
 Introduce the game (on first run. skip on subsequent runs using do while) and ask for player's number
 
@@ -17,18 +16,54 @@ Begin loop (if high and low are good. If not chastise and restart game)
         restart loop
     elif low:
         adjust low
+        restart loop
     elif high:
         adjust high
         restart loop
     else (invalid):
         instruct player on rules
         restart loop (with original guess)
-        
-"""
+*/
 
 public class MindReader {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continuePlaying;
+        System.out.println("\nWelcome to the Mind Readers Tent.\nHere you will think of a number and I will guess it.\n\nIf the number is too high, type h.\nIf it's too low, type l.\nWhen I get it correct, type c.\n\nLet us begin.");
         
+        do {
+            int high = 100;
+        int low = 1;
+            for (int i = 0; i <= 7; i++) { // i/count starts at zero and increases each round. If i is greater than seven, the user has been given invalid guesses and we start over
+                int currentGuess = (low + high) / 2;
+                System.out.println("\n\n\nIs your number " + currentGuess + "?");
+                String reply = scanner.nextLine();
+
+                if (reply.equals("c")) {
+                    System.out.println("\nI've conquered another mind.");
+                    break;
+                }
+                else if (reply.equals("l")) {
+                    low = currentGuess + 1;
+                }
+                else if (reply.equals("h")) {
+                    high = currentGuess - 1;
+                }
+                else {
+                    System.out.println("\nI'm not quite sure what you're trying to tell me. You need to say l, h, or c\n");
+                    break;
+                }
+            } 
+            System.out.println("\nIt seems our game has come to an end.\nPlay again? y/n");
+            String playAgainPrompt = scanner.nextLine();
+
+            if (playAgainPrompt.equals("y")) {
+                continuePlaying = true;
+            }
+            else {
+                continuePlaying = false;
+            }
+        } while (continuePlaying);
+        scanner.close();
     }
 }
