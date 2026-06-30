ff16a3a4723be436122799a72473daad6a6fe687
Oliver Rich-Jackson <10Oliver28@gmail.com>
2026-06-29 18:16:21 -0600

    Polish toolbox

--- Toolbox.java
+++ Toolbox.java
@@ -2,16 +2,20 @@ import java.util.Random;
 
 public class Toolbox {
     int square(int n) { // returns the square of n
-            return n*n;
-        }
+        return n*n;
+    }
     int max(int a, int b) { // returns the greater of two ints
         return (a > b) ? a : b;
     }
     boolean isEven(int n) { // returns whether an int is even
-        return (n % 2 == 0) ? true : false;
+        return (n % 2 == 0);
     }
     void describe(int n) { // describes an int in terms of its square, whether it's greater or less than 10, and if it's even
-        System.out.println("\n" + n + "^2: " + square(n) + " | " + "Greater Value: " + max(n, 10) + " | " + n + " Is Even: " + isEven(n));
+        String squareDesc = n + "^2: " + square(n);
+        String maxDesc = "Greater Value: " + max(n, 10);
+        String evenDesc = n + " Is Even: " + isEven(n);
+
+        System.out.println("\n" + squareDesc + " | " + maxDesc + " | " + evenDesc);
     }
     
     public static void main(String[] args) {
